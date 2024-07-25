e040fd64c5680f03a1c8d0c7cae372506cf299b9
Cybrux-365 <laro1laro34@gmail.com>
2024-07-25 09:12:22 +0100

    Test

--- wr.c
+++ wr.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 #include <fcntl.h>
-void ft_fd (int *p)
+void ft_wr (int *p)
 {
     write (*p, "new \n", 5);
 }
@@ -8,7 +8,7 @@ int main (void)
 {
     int y;
     y = open ("file.txt", O_WRONLY | O_CREAT );
-    ft_fd ( &y);
+    ft_wr (&y);
 
     return (0);
 }
